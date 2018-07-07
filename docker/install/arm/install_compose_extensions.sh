4a43d4f4befc3864ddf116f200dbf3df566c508c
Andreas Elser <andreas.elser@gmail.com>
2018-07-07 08:37:07 +0200

    Changed the script to install Docker Compose via pip

--- docker/install/arm/install_compose_extensions.sh
+++ docker/install/arm/install_compose_extensions.sh
@@ -3,14 +3,11 @@
 # Get all packages and install the latest updates
 apt update && apt upgrade -y;
 
+# Install python and the package manager
+apt install -y python python-pip;
+
 # Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
-
-# Apply executable permissions to the binary
-chmod +x /usr/local/bin/docker-compose;
-
-# Install command completion
-curl -L https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-com$;
+pip install docker-compose;
 
 # Display messages
 docker-compose --version;
