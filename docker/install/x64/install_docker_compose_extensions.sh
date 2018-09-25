e67c50999435a6165230f0da08a4f627d04b8e9b
Andreas Elser <andreas.elser@balluff.de>
2018-09-25 14:46:31 +0200

    Changed usermod command and echo output in script

--- docker/install/x64/install_docker_compose_extensions.sh
+++ docker/install/x64/install_docker_compose_extensions.sh
@@ -24,18 +24,17 @@ apt update && apt install -y docker-ce;
 
 # Docker post installation
 groupadd docker;
-usermod -aG docker $USER;
 
 # Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
+curl -L https://github.com/docker/compose/releases/download/1.22/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
 
 # Apply executable permissions to the binary
 chmod +x /usr/local/bin/docker-compose;
 
 # Install command completion
-curl -L https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-com$
+curl -L https://raw.githubusercontent.com/docker/compose/1.22/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-com$
 
 # Display messages
 docker --version;
 docker-compose --version;
-echo "The current user $USER is added to use docker as non-root. Please login again for usage";
+echo "Docker and Docker Compose is now installed. If don't want to enter sudo docker every time you can add your current user via usermod -aG docker $USER and re-login to overtake the changes.";
