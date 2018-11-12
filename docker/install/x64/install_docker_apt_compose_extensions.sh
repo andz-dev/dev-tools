bbb9d65064d68b10d9643928031ad0c42cf4e62b
Andreas Elser <andreas.elser@gmail.com>
2018-11-12 21:06:07 +0100

    Changed Docker installation script and updated compose version. Create a new installation script to install docker from the apt repo.

--- docker/install/x64/install_docker_apt_compose_extensions.sh
+++ docker/install/x64/install_docker_apt_compose_extensions.sh
@@ -4,23 +4,7 @@
 apt update && apt upgrade -y;
 
 # Install necessary packages
-apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common;
-
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
-
-# Add stable repository
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable";
-
-# Install Docker
-apt update && apt install -y docker-ce;
+apt install -y docker.io;
 
 # Docker post installation
 groupadd docker;
